2298ef9561aa09f5b5b93a87cb673dd4d7f5d97a
Daniel Almeida <danielga100@hotmail.com>
2014-02-04 19:56:18 +0000

    Trying to fix the screw up on the submodule.

--- gmod-module-base/example/BuildProjects.bat
+++
@@ -1,4 +0,0 @@
-premake4 --os=windows --platform=x32 --file=BuildProjects.lua vs2010
-premake4 --os=macosx --platform=universal32 --file=BuildProjects.lua gmake
-premake4 --os=linux --platform=x32 --file=BuildProjects.lua gmake
-pause
