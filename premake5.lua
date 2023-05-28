43a9c812786f97757c63a3d4cb77caabaec52133
Daniel <danielga@outlook.pt>
2023-05-28 17:51:54 +0100

    Always build CryptoPP with provided files

--- premake5.lua
+++ premake5.lua
@@ -1,11 +1,6 @@
 PROJECT_GENERATOR_VERSION = 2
 
 newoption({
-	trigger = "compile-cryptopp",
-	description = "Compile cryptopp along with the modules"
-})
-
-newoption({
 	trigger = "gmcommon",
 	description = "Sets the path to the garrysmod_common (https://github.com/danielga/garrysmod_common) directory",
 	value = "path to garrysmod_common directory"
@@ -24,7 +19,9 @@ CreateWorkspace({name = "crypt"})
 	CreateProject({serverside = true, manual_files = true})
 		IncludeLuaShared()
 		defines("CRYPTOPP_ENABLE_NAMESPACE_WEAK=1")
+		links("cryptopp")
 		includedirs({
+			CRYPTOPP_DIRECTORY .. "/include",
 			SOURCE_DIRECTORY .. "/common",
 			SOURCE_DIRECTORY .. "/module"
 		})
@@ -34,23 +31,13 @@ CreateWorkspace({name = "crypt"})
 			SOURCE_DIRECTORY .. "/module/*.hpp",
 			SOURCE_DIRECTORY .. "/module/*.cpp"
 		})
-
-		filter({"system:windows", "options:not compile-cryptopp"})
-			includedirs(CRYPTOPP_DIRECTORY .. "/include")
-			libdirs(CRYPTOPP_DIRECTORY .. "/lib")
-			links("cryptopp")
-
-		filter({"system:linux or macosx", "options:not compile-cryptopp"})
-			linkoptions("-Wl,-Bstatic,-lcryptopp,-Bdynamic")
-
-		filter("options:compile-cryptopp")
-			includedirs(CRYPTOPP_DIRECTORY .. "/include")
-			links("cryptopp")
 
 	CreateProject({serverside = false, manual_files = true})
 		IncludeLuaShared()
 		defines("CRYPTOPP_ENABLE_NAMESPACE_WEAK=1")
+		links("cryptopp")
 		includedirs({
+			CRYPTOPP_DIRECTORY .. "/include",
 			SOURCE_DIRECTORY .. "/common",
 			SOURCE_DIRECTORY .. "/module"
 		})
@@ -61,41 +48,31 @@ CreateWorkspace({name = "crypt"})
 			SOURCE_DIRECTORY .. "/module/*.cpp"
 		})
 
-		filter({"system:windows", "options:not compile-cryptopp"})
-			includedirs(CRYPTOPP_DIRECTORY .. "/include")
-			libdirs(CRYPTOPP_DIRECTORY .. "/lib")
-			links("cryptopp")
+	project("cryptopp")
+		kind("StaticLib")
+		includedirs({
+			CRYPTOPP_DIRECTORY .. "/include/cryptopp",
+			CRYPTOPP_DIRECTORY .. "/src"
+		})
+		files({
+			CRYPTOPP_DIRECTORY .. "/include/cryptopp/*.h",
+			CRYPTOPP_DIRECTORY .. "/src/*.cpp"
+		})
+		vpaths({
+			["Header files"] = CRYPTOPP_DIRECTORY .. "/*.h",
+			["Source files"] = CRYPTOPP_DIRECTORY .. "/*.cpp"
+		})
 
-		filter({"system:linux or macosx", "options:not compile-cryptopp"})
-			linkoptions("-Wl,-Bstatic,-lcryptopp,-Bdynamic")
-
-		filter("options:compile-cryptopp")
-			includedirs(CRYPTOPP_DIRECTORY .. "/include")
-			links("cryptopp")
-
-	if _OPTIONS["compile-cryptopp"] then
-		project("cryptopp")
-			kind("StaticLib")
-			includedirs({
-				CRYPTOPP_DIRECTORY .. "/include/cryptopp",
-				CRYPTOPP_DIRECTORY .. "/src"
-			})
-			files({
-				CRYPTOPP_DIRECTORY .. "/include/cryptopp/*.h",
-				CRYPTOPP_DIRECTORY .. "/src/*.cpp"
-			})
-			vpaths({
-				["Header files"] = CRYPTOPP_DIRECTORY .. "/*.h",
-				["Source files"] = CRYPTOPP_DIRECTORY .. "/*.cpp"
-			})
-
-			filter("configurations:Release")
-				defines("NDEBUG")
-	end
+		filter("configurations:Release")
+			defines("NDEBUG")
 
 	project("testing")
 		kind("ConsoleApp")
-		includedirs(SOURCE_DIRECTORY .. "/common")
+		links("cryptopp")
+		includedirs({
+			CRYPTOPP_DIRECTORY .. "/include",
+			SOURCE_DIRECTORY .. "/common"
+		})
 		files({
 			SOURCE_DIRECTORY .. "/common/*.hpp",
 			SOURCE_DIRECTORY .. "/common/*.cpp",
@@ -105,15 +82,3 @@ CreateWorkspace({name = "crypt"})
 			["Header files/*"] = SOURCE_DIRECTORY .. "/*.hpp",
 			["Source files/*"] = SOURCE_DIRECTORY .. "/*.cpp"
 		})
-
-		filter({"system:windows", "options:not compile-cryptopp"})
-			includedirs(CRYPTOPP_DIRECTORY .. "/include")
-			libdirs(CRYPTOPP_DIRECTORY .. "/lib")
-			links("cryptopp")
-
-		filter({"system:linux or macosx", "options:not compile-cryptopp"})
-			linkoptions("-Wl,-Bstatic,-lcryptopp,-Bdynamic")
-
-		filter("options:compile-cryptopp")
-			includedirs(CRYPTOPP_DIRECTORY .. "/include")
-			links("cryptopp")
